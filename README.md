66748d553c5b88ae0b184a411ce42cb97aee94aa
adrielmorales <adrielmoral007@gmail.com>
2021-07-01 00:26:55 -0500

    Hello!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cadett
+Cadett is a Vowel Counting Program for the PC. Scan Word Docs, Text Files (.txt) And all types of files. It does not have a GUI and it also has universal support for any kind of operating system.
---
+++ cmake-build-debug/CMakeFiles/Cadett.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cadett.dir/function.c.obj"
+  "CMakeFiles/Cadett.dir/main.c.obj"
+  "Cadett.exe"
+  "Cadett.exe.manifest"
+  "Cadett.pdb"
+  "libCadett.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Cadett.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
